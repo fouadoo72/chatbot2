2ff2841e6b557bc9252f80f2afdd65654f246d23
unknown <fouadfouad72@gmail.com>
2022-04-21 21:30:46 +0300

    releas

---
+++ app.py
@@ -0,0 +1,12 @@
+from crypt import methods
+from flask import Flask ,jsonify,request
+import time 
+app =Flask (__name__)
+@app.route("/bot",methods=["POST"])
+def response():
+    query =dict (request.form)['query']
+    result =query + "fofofofofo"+time.ctime()
+    return jsonify ({"response":result})
+
+    if __name__ =="__main__":
+        app.run(host="0.0.0.0",)
